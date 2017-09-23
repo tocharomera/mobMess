8badb2884cea1f5fae1cc7d2b2bc6457f4d3f38a
tocharomera <nevertalking@hotmail.com>
2017-09-23 13:54:54 +0200

    last

--- public/panel.js
+++ public/panel.js
@@ -1,41 +1,40 @@
-
 $(document).ready(init)
 var socket
 var cells
 
-function init(){
+function init() {
   socket = io('/panel')
   document.getElementById('request').addEventListener('click', () => {
-	if (screenfull.enabled) {
-		screenfull.request();
-	} else {	// Ignore or do something else
-	}
+    if (screenfull.enabled) {
+      screenfull.request();
+    } else { // Ignore or do something else
+    }
   });
 
-  $("#clear").click(function(){
-      var code = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  $("#clear").click(function() {
+    var code = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-  displayBits(code)
-      $('#code').text("[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]")
+    displayBits(code)
+    $('#code').text("[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]")
   });
 
   document.getElementById('exit').addEventListener('click', () => {
-	screenfull.exit();
+    screenfull.exit();
   });
 
   cells = []
-  for(var i=0;i<35;i++){
+  for (var i = 0; i < 35; i++) {
     //select every cell
-    var cell = $('#c'+i)
+    var cell = $('#c' + i)
     cells.push(cell)
   }
 
   //display how many devices are associated to each pixel
-  socket.on('connCount',function(connCount){
-    for(var i=0;i<35;i++){
-      if(connCount[i] > 0){
+  socket.on('connCount', function(connCount) {
+    for (var i = 0; i < 35; i++) {
+      if (connCount[i] > 0) {
         cells[i].addClass('connected')
-      }else{
+      } else {
         cells[i].removeClass('connected')
       }
     }
@@ -44,48 +43,48 @@ function init(){
 
 
 
-  socket.on('pixels',function(code){
-//socket.emit('pixels',code)
+  socket.on('pixels', function(code) {
+    socket.emit('pixels', code)
     displayBits(code)
   })
-  socket.on('finnished',function(){
+  socket.on('finnished', function() {
     console.log("finished");
     $("#msgsend").removeAttr('disabled');
     $('#msgsend').val("Send")
 
   })
 
-  $('table.table td').click(function(){
+  $('table.table td').click(function() {
     $(this).toggleClass('selected')
     var code = getUpdate()
-  //  displayBits(code)
-    socket.emit('pixels',code)
+     displayBits(code)
+    socket.emit('pixels', code)
   })
-  $(document).keypress(function (e) {
-   var key = e.which;
-   if(key == 13)  // the enter key code
+  $(document).keypress(function(e) {
+    var key = e.which;
+    if (key == 13) // the enter key code
     {
       $('#msgsend').trigger("click");
     }
   });
   //message forme
-  $('#msgsend').click(function(){
+  $('#msgsend').click(function() {
 
     $('#msgsend').val("Please Wait...")
     $('#msgsend').attr('disabled', 'disabled');
 
 
     var msg = $('#msginput').val()
-    $.get('/msg/'+msg)
+    $.get('/msg/' + msg)
 
   })
 }
 //display the code of the character display
-function getUpdate(){
-var bits = [];
-  for(var i=0;i<cells.length;i++){
+function getUpdate() {
+  var bits = [];
+  for (var i = 0; i < cells.length; i++) {
     var cell = cells[i]
-    var bit  = cell.hasClass('selected')
+    var bit = cell.hasClass('selected')
     //http://stackoverflow.com/questions/7820683/convert-boolean-result-into-number-integer#7820695
     bits.push(bit + false)
 
@@ -94,18 +93,18 @@ var bits = [];
   return bits
 }
 
-function displayBits(bits){
+function displayBits(bits) {
   var code = ''
-  for(var i=0;i<bits.length;i++){
+  for (var i = 0; i < bits.length; i++) {
     var cell = cells[i]
-    var bit  = bits[i]
-    if(bit){
+    var bit = bits[i]
+    if (bit) {
       cell.addClass('selected')
 
-    }else{
+    } else {
       cell.removeClass('selected')
     }
   }
-  code = '['+bits.join(',')+']'
+  code = '[' + bits.join(',') + ']'
   $('#code').text(code)
 }
